0109d28dcbb5346f1c35ecb2ec65903d0e2a0bdb
Silvia <silvia.garbas@gmail.com>
2023-06-08 22:24:28 +0200

    Added <p>

--- src/index.ts
+++ src/index.ts
@@ -5,40 +5,35 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-const PORT = process.env.PORT || 4000;
-const version = '1.0.0';
+const PORT = process.env.PORT || 3000;
 
-program.option('-v, --version');
-program.parse();
-const options = program.opts();
 
-if (options.version) {
-  console.log('Version ' + version);
-  process.exit(0);
-}
 
+const a = 6;
+const b = 3;
 const addition = (a: number, b: number) => a + b;
 const substraction = (a: number, b: number) => a - b;
 const division = (a: number, b: number) => a / b;
 const multiplication = (a: number, b: number) => a * b;
 
 const server = http.createServer((req, res) => {
-  console.log('Server ok');
-  const { pathname, query} = url.parse(req.url!);
-
-
-  if (req.method !== 'GET') {
-    server.emit('error', new Error('Invalid method'));
+  if (!req.url) {
+    server.emit('error', new Error('No url in the request'));
     return;
   }
 
-  res.write(`<h1>Resultados ${pathname!.toUpperCase()}</h1>`);
-  res.write(req.method);
-  res.write(req.url);
-  res.write(`${addition(6, 3)}`);
-  res.write(`${substraction(6, 3)}`);
-  res.write(`${division(6, 3)}`);
-  res.write(`${multiplication(6, 3)}`);
+  const { pathname, query } = url.parse(req.url);
+
+console.log(query)
+
+
+
+  res.write(`<h1>Calculator</h1>`);
+
+  res.write(`<p>${ addition(a, b)}</p>`);
+  res.write(`<p>${substraction(a, b)}</p>`);
+  res.write(`<p>${division(a, b)}</p>`);
+  res.write(`<p>${multiplication(a, b)}</p>`);
   res.end();
 });
 
