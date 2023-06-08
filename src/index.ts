18fb47d34e973cd0c6f80dfc3421a19cb603958f
Silvia <silvia.garbas@gmail.com>
2023-06-09 00:01:58 +0200

    Unsolved error with query

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,8 @@ const PORT = process.env.PORT || 3000;
 
 const a = 6;
 const b = 3;
+
+
 const addition = (a: number, b: number) => a + b;
 const substraction = (a: number, b: number) => a - b;
 const division = (a: number, b: number) => a / b;
@@ -22,19 +24,29 @@ const server = http.createServer((req, res) => {
     return;
   }
 
-  const { pathname, query } = url.parse(req.url);
+  const { pathname, query} = url.parse(req.url!)
 
-console.log(query)
+  //  If(pathname !== '/calculator'){
+  //   res.statusCode = 404;
+  //   res.end('Error. Not found.');
+  //   return;
+  // }
+
+// if(query  === '/calculator?a=6&b=3')
 
 
-
-  res.write(`<h1>Calculator</h1>`);
+   res.write(`<p>Calculator. Query es: ${query}</p>`);
 
   res.write(`<p>${ addition(a, b)}</p>`);
   res.write(`<p>${substraction(a, b)}</p>`);
   res.write(`<p>${division(a, b)}</p>`);
   res.write(`<p>${multiplication(a, b)}</p>`);
+
+
   res.end();
+
+
+
 });
 
 server.listen(PORT);
